ec7cee4f178561787be93f367f70bf1a1b034a4b
Paulo <paulo.bressan@outlook.com>
2018-07-31 23:30:29 -0300

    Init, restify, typescript, conf server, conf router

---
+++ dist/main.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//importando o restify para criar o servidor
+const restify = require("restify");
+//Configurando o servidor, nome e versão
+const server = restify.createServer({
+    name: 'meat-api',
+    version: '1.0.0'
+});
+//Configurando um plugin para capturar os valores passado na url
+server.use(restify.plugins.queryParser());
+//criando um metodo get, uma rota valida
+server.get('/info', (req, res, next) => {
+    //criando uma resposta em json para quem o chamou
+    res.json({
+        //capturando dados do client
+        browser: req.userAgent(),
+        //capturando o metodo http tomado para realizar a requisição
+        Method: req.method,
+        //capturando a url
+        url: req.url,
+        //camminho completo
+        path: req.path(),
+        //parametros passados
+        params: req.query
+    });
+    //Sempre que terminarmos o codigo no callback da rota, 
+    // chamar o next para indicar que o metodo terminou
+    return next();
+});
+//Esctuando o servidor na porta 3000
+server.listen(3000, () => {
+    console.log('API is running on http://localhost:3000');
+});
