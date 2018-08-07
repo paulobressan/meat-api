6a92bcf769c27e7e8fc0ce51e284babe9007e56f
Paulo Bressan <paulo.bressan>
2018-08-07 13:48:54 -0300

    Metodo Put implementado

--- dist/users/users.router.js
+++ dist/users/users.router.js
@@ -32,6 +32,41 @@ class UsersRouter extends router_1.Router {
                 return next();
             });
         });
+        application.get('/users/byName/:name', (req, resp, next) => {
+            users_model_1.User.find({ name: req.params.name }).then(user => {
+                if (user) {
+                    resp.json(user);
+                    return next();
+                }
+                resp.json(404);
+                return next();
+            });
+        });
+        //realizando um put
+        //o id do documento é imutavel depois de ser criado, porem podemos definir o id antes de ser criado
+        application.put('/users/:id', (req, resp, next) => {
+            const options = { overwrite: true };
+            //por padrão o update sempre altera os dados parcialmente e no caso abaixo
+            //queremos alterar todas prop do documento com as prop do body recebida
+            users_model_1.User.update({ _id: req.params.id }, req.body, options)
+                //o update retorna uma query e temos que executar essa query para poder aplicar a promisse
+                .exec()
+                .then(result => {
+                //Se o documento foi subscrevido
+                if (result.n) {
+                    //Se o documento foi alterado, vamos retornar o documento alterado
+                    return users_model_1.User.findById(req.params.id);
+                }
+                else {
+                    //Se não aplicar a alteração no documento é porque o documento não existe.
+                    resp.send(404);
+                }
+            })
+                .then(user => {
+                resp.json(user);
+                return next();
+            });
+        });
     }
 }
 //quem chamar essa classe vai receber uma instancia pronta para utilizar
--- users/users.router.ts
+++ users/users.router.ts
@@ -1,12 +1,12 @@
 import * as restify from 'restify';
-import {Router} from '../common/router';
+import { Router } from '../common/router';
 //importando o model de usuarios
-import {User} from '../users/users.model';
+import { User } from '../users/users.model';
 
 //classe que define rotas de usuarios
-class UsersRouter extends Router{
-    applyRoutes(application: restify.Server){
-        application.get('/users', (req,resp, next)=>{
+class UsersRouter extends Router {
+    applyRoutes(application: restify.Server) {
+        application.get('/users', (req, resp, next) => {
             User.find().then(users => {
                 resp.json(users);
                 return next();
@@ -15,7 +15,7 @@ class UsersRouter extends Router{
 
         application.get('/users/:id', (req, resp, next) => {
             User.findById(req.params.id).then(user => {
-                if(user){
+                if (user) {
                     resp.json(user);
                     return next();
                 }
@@ -34,6 +34,43 @@ class UsersRouter extends Router{
                 return next();
             })
         });
+
+        application.get('/users/byName/:name', (req, resp, next) => {
+            User.find({ name: req.params.name }).then(user => {
+                if (user) {
+                    resp.json(user);
+                    return next();
+                }
+                resp.json(404);
+                return next();
+            })
+        });
+
+        //realizando um put
+        //o id do documento é imutavel depois de ser criado, porem podemos definir o id antes de ser criado
+        application.put('/users/:id', (req, resp, next) => {
+            const options = { overwrite: true }
+            //por padrão o update sempre altera os dados parcialmente e no caso abaixo
+            //queremos alterar todas prop do documento com as prop do body recebida
+            User.update({ _id: req.params.id }, req.body, options)
+                //o update retorna uma query e temos que executar essa query para poder aplicar a promisse
+                .exec()
+                .then(result => {
+                    //Se o documento foi subscrevido
+                    if (result.n) {
+                        //Se o documento foi alterado, vamos retornar o documento alterado
+                        return User.findById(req.params.id);
+                    } else {
+                        //Se não aplicar a alteração no documento é porque o documento não existe.
+                        resp.send(404);
+                    }
+                })
+                .then(user => {
+                    resp.json(user);
+                    return next();
+                })
+
+        })
     }
 }
 
