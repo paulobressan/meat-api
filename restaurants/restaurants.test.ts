eb08a08f4b8dcdfbb60912b1a3ce53bc9f5e98ca
Paulo Bressan <paulo.bressan@idealeware.com.br>
2018-12-12 17:12:10 -0200

    Implementando tests restaurants e menu

--- restaurants/restaurants.test.ts
+++ restaurants/restaurants.test.ts
@@ -11,3 +11,57 @@ test('get /restaurants', () => {
             expect(response.body.items).toBeInstanceOf(Array)
         }).catch(fail)
 })
+
+test('post /restaurants', () => {
+    return request(address)
+        .post('/restaurants')
+        .send({
+            name: 'Restaurants Teste'
+        })
+        .then(response => {
+            expect(response.status).toBe(200)
+            expect(response.body._id).toBeDefined()
+            expect(response.body.name).toBe('Restaurants Teste')
+        }).catch(fail)
+})
+
+test('patch /restaurants', () => {
+    return request(address)
+        .post('/restaurants')
+        .send({
+            name: 'Restaurants Teste'
+        })
+        .then(response =>
+            request(address)
+                .patch(`/restaurants/${response.body._id}`)
+                .send({
+                    name: 'Restaurants Teste 2'
+                }))
+        .then(response => {
+            expect(response.status).toBe(200)
+            expect(response.body._id).toBeDefined()
+            expect(response.body.name).toBe('Restaurants Teste 2')
+        })
+})
+
+test('patch /restaurants menu', () => {
+    return request(address)
+        .post('/restaurants')
+        .send({
+            name: 'Restaurants Teste'
+        })
+        .then(response =>
+            request(address)
+                .put(`/restaurants/${response.body._id}/menu`)
+                .send({
+                    name: 'X-TUDO',
+                    price: 20
+                }))
+        .then(response => {
+            expect(response.status).toBe(200)
+            expect(response.body._id).toBeDefined()
+            expect(response.body.menu).toBeInstanceOf(Array)
+            expect(response.body.menu[0].name).toBe('X-TUDO')
+            expect(response.body.menu[0].price).toBe(20)
+        })
+})
