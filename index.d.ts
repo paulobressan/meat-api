733938c9f7ff72892d2b35ad25e3152b701c7b51
Paulo Bressan <paulo.bressan@outlook.com>
2018-12-24 22:35:32 -0200

    Typescript declarando novas props em modulos existentes.

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,10 @@
 import { User } from './users/users.model'
 
+//No modulo restify, vamos modificar
 declare module 'restify' {
+    //Na interface Request vamos adicionar uma nova prop
     export interface Request {
+        //Nova Prop adicionada do tipo User
         authenticated: User
     }
 }
